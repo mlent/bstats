a03b4c77dd7c1252d0127c7d47cc4da2c68b7a72
Monica Lent <lent.monica@gmail.com>
2017-10-28 23:55:27 +0200

    Implement basic post word counting.

--- src/lib/Posts.hs
+++ src/lib/Posts.hs
@@ -1,8 +1,13 @@
 module Posts
   ( findPosts
+  , isMarkdown
   ) where
 
 import           System.Directory
+import           System.FilePath
 
 findPosts :: FilePath -> IO [FilePath]
 findPosts = getDirectoryContents
+
+isMarkdown :: FilePath -> Bool
+isMarkdown fp = takeExtension fp == ".md"
