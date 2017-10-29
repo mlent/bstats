b963eedcf2b861a417b8cfcaa9527c8f9b2af093
Monica Lent <lent.monica@gmail.com>
2017-10-29 12:03:30 +0100

    Separate body and frontmatter from post, write tests.

--- src/lib/Posts.hs
+++ src/lib/Posts.hs
@@ -1,13 +1,44 @@
 module Posts
   ( findPosts
   , isMarkdown
+  , extractFrontmatter
+  , extractPostBody
+  , splitFrontMatterAndBody
   ) where
 
 import           System.Directory
 import           System.FilePath
+import           Data.List
+import           Data.List.Split
+import           Data.Char
 
 findPosts :: FilePath -> IO [FilePath]
 findPosts = getDirectoryContents
 
 isMarkdown :: FilePath -> Bool
 isMarkdown fp = takeExtension fp == ".md"
+
+splitFrontMatterAndBody :: String -> (String, String)
+splitFrontMatterAndBody content = (frontmatter, body)
+  where
+    splitBody = map trim . filter notEmpty $ splitOn "+++" content
+    frontmatter = head splitBody
+    body = unwords $ tail splitBody
+
+trim :: String -> String
+trim = trimL . trimR
+
+trimL :: String -> String
+trimL = dropWhile isSpace
+
+trimR :: String -> String
+trimR = reverse . trimL . reverse
+
+notEmpty :: String -> Bool
+notEmpty str = str /= ""
+
+extractFrontmatter :: String -> String
+extractFrontmatter = fst . splitFrontMatterAndBody
+
+extractPostBody :: String -> String
+extractPostBody = snd . splitFrontMatterAndBody
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,35 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+module Spec
+( suite
+) where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Posts
+
+content :: String
+content = "+++ category=[\"travel\"] +++ This is my body."
+
+suite :: TestTree
+suite = testGroup "Posts"
+  [
+    testGroup "extractFrontmatter"
+    [
+      testCase "extracts the frontmatter from a post" extractFrontmatter1
+    ]
+  , testGroup "extractPostBody"
+    [
+      testCase "extracts the body from a post" extractPostBody1
+    ]
+  ]
+
+extractFrontmatter1 :: Assertion
+extractFrontmatter1 = expected @=? actual
+  where
+    actual = extractFrontmatter content
+    expected = "category=[\"travel\"]"
+
+extractPostBody1 :: Assertion
+extractPostBody1 = expected @=? actual
+  where
+    actual = extractPostBody content
+    expected = "This is my body."
