8a0cbbaff6ca9d6578bad4ad3495c855e13c7369
Monica Lent <lent.monica@gmail.com>
2017-10-31 11:16:48 +0100

    Format src and test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,31 +1,29 @@
 module Spec
-( suite
-) where
+  ( suite
+  ) where
 
-import Test.Tasty
-import Test.Tasty.HUnit
-import Data.Map
-import Posts
+import           Data.Map
+import           Posts
+import           Test.Tasty
+import           Test.Tasty.HUnit
 
 content :: String
 content = "+++\ncategory=[\"travel\"]\n+++\nThis is my body."
 
 suite :: TestTree
-suite = testGroup "Posts"
-  [
-    testGroup "extractFrontmatter"
-    [
-      testCase "extracts the frontmatter from a post" extractFrontmatter1
+suite =
+  testGroup
+    "Posts"
+    [ testGroup
+        "extractFrontmatter"
+        [testCase "extracts the frontmatter from a post" extractFrontmatter1]
+    , testGroup
+        "extractPostBody"
+        [testCase "extracts the body from a post" extractPostBody1]
+    , testGroup
+        "countWords"
+        [testCase "count number of occurrences of each word" countWords1]
     ]
-  , testGroup "extractPostBody"
-    [
-      testCase "extracts the body from a post" extractPostBody1
-    ]
-  , testGroup "countWords"
-    [
-      testCase "count number of occurrences of each word" countWords1
-    ]
-  ]
 
 extractFrontmatter1 :: Assertion
 extractFrontmatter1 = expected @=? actual
